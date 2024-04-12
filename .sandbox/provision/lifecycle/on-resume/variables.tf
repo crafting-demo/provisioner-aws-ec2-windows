c43fc346d569c68a2ed5d4503ec948bd7dd67a24
Yuan <whywhy36@gmail.com>
2024-04-12 04:31:22 +0000

    use symb link for variables.tf

--- .sandbox/provision/lifecycle/on-resume/variables.tf
+++ .sandbox/provision/lifecycle/on-resume/variables.tf
@@ -1,13 +1,1 @@
-variable "launch_template_name" {
-  #default = "sandbox-windows-vm"
-  default = "crafting-windows-vm"
-}
-
-variable "launch_template_version" {
-  default = "1"
-}
-
-variable "keypair_file" {
-  #default = "/run/sandbox/fs/secrets/shared/sandbox-shared.pem"
-  default = "/run/sandbox/fs/secrets/shared/ec2-keypair-shared.pem"
-}
+../common/variables.tf
