82c785a4e941fee88d6bbc5d46e3c4e7bc2bde9d
Yuan <whywhy36@gmail.com>
2024-04-17 20:52:15 +0000

    update

--- .sandbox/provision/lifecycle/on-create/entry.sh
+++ .sandbox/provision/lifecycle/on-create/entry.sh
@@ -4,3 +4,5 @@ terraform init -no-color -input=false >./on-create.log 2>&1
 terraform apply --auto-approve -no-color -input=false >./on-create.log 2>&1
 
 jq .outputs ../terraform.tfstate
+
+# cs restart rdp 
--- .sandbox/provision/lifecycle/on-resume/entry.sh
+++ .sandbox/provision/lifecycle/on-resume/entry.sh
@@ -4,3 +4,5 @@ terraform init -no-color -input=false >./on-resume.log 2>&1
 terraform apply --auto-approve -no-color -input=false >./on-resume.log 2>&1
 
 jq .outputs ../terraform.tfstate
+
+# cs restart rdp 
