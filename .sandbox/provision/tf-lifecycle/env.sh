5f95543ddb9448bdc32385f19b99e28afa08f8cb
Yuan Zhuang <whywhy36@gmail.com>
2024-04-26 00:24:30 +1200

    fix

--- .sandbox/provision/tf-lifecycle/env.sh
+++ .sandbox/provision/tf-lifecycle/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-public_key=$(ssh-keygen -y -f ${EC2_SSH_KEY_FILE:-/run/sandbox/fs/secrets/shared/employ-shared.pem})
+public_key=$(ssh-keygen -y -f ${EC2_SSH_KEY_FILE:-/run/sandbox/fs/secrets/shared/sandbox-shared.pem})
 cat <<EOF
 {
     "sandbox_id": "$SANDBOX_ID",
