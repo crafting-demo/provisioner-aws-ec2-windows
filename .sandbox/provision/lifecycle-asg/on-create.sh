41c527da775bbb680bedadd77e4643b42616ce30
Yuan Zhuang <whywhy36@gmail.com>
2024-04-29 13:16:56 +1200

    update

--- .sandbox/provision/lifecycle-asg/on-create.sh
+++ .sandbox/provision/lifecycle-asg/on-create.sh
@@ -4,35 +4,40 @@
 : $(AVAILABILITY_ZONE:=us-east-2a)
 : $(MAX_RETRIES:=3)
 : $(VOLUME_SIZE:=10)
+: $(EC2_SSH_KEY_FILE:=/run/sandbox/fs/secrets/shared/sandbox-shared.pem)
 
 source ./common.sh
 
 # adjust the terminal output settings 
 redirect_output on-create.log
 
-# TODO: need to lock and prevent race condition
-INSTANCE_IDS="$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --query "AutoScalingGroups[].Instances[].InstanceId" --output text)"
+INSTANCE_IDS="$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG_NAME --no-paginate --query "AutoScalingGroups[].Instances[].InstanceId" --output text)"
 INSTANCE_ID="$(echo $INSTANCE_IDS | awk '{print $1}')"
 
-aws autoscaling detach-instances --instance-ids $INSTANCE_ID --auto-scaling-group-name $ASG_NAME --no-should-decrement-desired-capacity
 aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Sandbox,Value=$SANDBOX_NAME --tags Key=SandboxID,Value=$SANDBOX_ID
+aws autoscaling detach-instances --instance-ids $INSTANCE_ID --auto-scaling-group-name $ASG_NAME --no-should-decrement-desired-capacity
+
 INSTANCE="$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query 'Reservations[0].Instances[0]' | jq '.')"
 PASSWORD_DATA="$(aws ec2 get-password-data --instance-id $INSTANCE_ID)"
 
 retry_count=0
 while [[ $retry_count -lt $MAX_RETRIES ]]; do
-    PASSWORD_DATA=$(aws ec2 get-password-data --instance-id $INSTANCE_ID | jq -r .PasswordData)
+    PASSWORD_DATA="$(aws ec2 get-password-data --instance-id $INSTANCE_ID | jq -r .PasswordData)"
     [[ -z "$PASSWORD_DATA" ]] || break
     retry_count=$((retry_count + 1))
     sleep 10
 done
 
-PASSWORD=$(echo $PASSWORD_DATA  | base64 -d | openssl pkeyutl -decrypt -inkey /run/sandbox/fs/secrets/shared/employ-temp.pem)
-PUBLIC_DNS=$(echo $INSTANCE | jq -r .NetworkInterfaces[0].Association.PublicDnsName)
-PUBLIC_IP=$(echo $INSTANCE | jq -r .NetworkInterfaces[0].Association.PublicIp)
+PASSWORD="$(echo $PASSWORD_DATA  | base64 -d | openssl pkeyutl -decrypt -inkey $EC2_SSH_KEY_FILE)"
+PUBLIC_DNS="$(echo $INSTANCE | jq -r .NetworkInterfaces[0].Association.PublicDnsName)"
+PUBLIC_IP="$(echo $INSTANCE | jq -r .NetworkInterfaces[0].Association.PublicIp)"
 
-VOLUME=$(aws ec2 create-volume --size $VOLUME_SIZE --availability-zone $AVAILABILITY_ZONE)
-VOLUME_ID=$(echo $VOLUME | jq -r .VolumeId)
+volume_info="${aws ec2 describe-volume --filters "Name=tag:SandboxID,Values=$SANDBOX_ID"}"
+VOLUME_ID="$(jq -r '.Volumes[0].VolumeId' <<< $volume_info)"
+if [[ $(jq '.Volumes | length' <<< "$volume_info") -gt 0 ]]; then
+    volume="$(aws ec2 create-volume --size $VOLUME_SIZE --availability-zone $AVAILABILITY_ZONE --tag-specification "ResourceType=volume,Tags=[{Key=SandboxID,Value=$SANDBOX_ID}]")"
+    VOLUME_ID=$(echo $volume | jq -r .VolumeId)
+fi
 aws ec2 wait volume-available --volume-ids $VOLUME_ID
 aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $INSTANCE_ID --device "/dev/xvdf"
 
--- .sandbox/provision/lifecycle-asg/on-delete.sh
+++ .sandbox/provision/lifecycle-asg/on-delete.sh
@@ -4,8 +4,8 @@ source ./common.sh
 
 redirect_output on-delete.log
 
-INSTANCE_ID=$(stored_instance_id)
-VOLUME_ID=$(stored_volume_id)
+INSTANCE_ID="$(stored_instance_id)"
+VOLUME_ID="$(stored_volume_id)"
 
 if [ -n "$INSTANCE_ID" ] ; then
     aws ec2 terminate-instances --instance-ids $INSTANCE_ID
