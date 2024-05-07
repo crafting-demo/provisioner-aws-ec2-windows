1e9783f9c9fdf86ee54ced8db565ab17541bfbe8
Yuan Zhuang <whywhy36@gmail.com>
2024-05-07 16:35:51 +1200

    update

---
+++ .sandbox/provision/lifecycle-asg/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cleanup leaked instances
+INSTANCE_IDS="$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:sandbox-asg,Values=true" --query 'Reservations[].Instances[?!(Tags[?Key==`aws:autoscaling:groupName`] || Tags[?Key==`SandboxID`])].InstanceId[]')"
+
+while IFS= read -r instance_id; do
+    echo "Processing instance ID: $instance_id"
+    aws ec2 terminate-instance --instance-ids "$instance_id"
+done < <(echo "$INSTANCE_IDS" | jq -r '.[]')
