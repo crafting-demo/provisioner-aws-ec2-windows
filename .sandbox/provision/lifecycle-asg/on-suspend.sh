178f38b519c0f18627b8149f15cffa90ead439a4
Yuan Zhuang <whywhy36@gmail.com>
2024-05-07 15:56:05 +1200

    fix

--- .sandbox/provision/lifecycle-asg/on-suspend.sh
+++ .sandbox/provision/lifecycle-asg/on-suspend.sh
@@ -10,6 +10,9 @@ INSTANCE_ID="$(get_instance_id)"
 VOLUME_ID="$(get_volume_id)"
 aws ec2 terminate-instances --instance-ids "$INSTANCE_ID"
 
+# we need to wait for termination's completed, in case of some race conditions: suspend and resume the sandbox immedidately.
+aws ec2 wait instance-terminated --instance-ids "$INSTANCE_ID"
+
 restore_stdout
 
 cat <<EOF
