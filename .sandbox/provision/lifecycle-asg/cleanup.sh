b7f4d4f874814990900e0a49439916bf5571ec00
Yuan Zhuang <whywhy36@gmail.com>
2024-05-09 11:20:28 +1200

    some updates

--- .sandbox/provision/lifecycle-asg/cleanup.sh
+++ .sandbox/provision/lifecycle-asg/cleanup.sh
@@ -2,9 +2,7 @@
 
 # cleanup leaked instances
 echo "Checking and cleaning up the leaked instances (if any)"
-INSTANCE_IDS="$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:sandbox-asg,Values=true" --query 'Reservations[].Instances[?!(Tags[?Key==`aws:autoscaling:groupName`] || Tags[?Key==`SandboxID`])].InstanceId[]')"
+INSTANCE_IDS="$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:sandbox-asg,Values=true" --query 'Reservations[].Instances[?!(Tags[?Key==`aws:autoscaling:groupName`] || Tags[?Key==`SandboxID`])].InstanceId[]') | jq -cMr '.[]'" 
+aws ec2 terminate-instances --instance-ids "${INSTANCE_IDS[@]}"
 
-while IFS= read -r instance_id; do
-    echo "Processing instance ID: $instance_id"
-    aws ec2 terminate-instances --instance-ids "$instance_id"
-done < <(echo "$INSTANCE_IDS" | jq -r '.[]')
+
--- .sandbox/provision/lifecycle-asg/common.sh
+++ .sandbox/provision/lifecycle-asg/common.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 : ${MAX_RETRIES:=10}
+: ${DEVICE_NAME:="/dev/xvdf"}
 
 function fatal() {
   echo "$@" >&2
@@ -18,13 +19,11 @@ function restore_stdout() {
 }
 
 function get_volume_id() {
-    local volume_info="$(aws ec2 describe-volumes --filters Name=tag:SandboxID,Values="$SANDBOX_ID")"
-    jq -cMr '.Volumes[0].VolumeId' <<< "$volume_info"
+    aws ec2 describe-volumes --filters Name=tag:SandboxID,Values="$SANDBOX_ID" --query 'Volumes[0].VolumeId' --output text
 }
 
 function get_instance_id() {
-    local instances_info="$(aws ec2 describe-instances --filters Name=tag:SandboxID,Values="$SANDBOX_ID" Name=instance-state-name,Values=running)"
-    jq -cMr '.Reservations[0].Instances[0].InstanceId' <<< "$instances_info"
+    aws ec2 describe-instances --filters Name=tag:SandboxID,Values="$SANDBOX_ID" Name=instance-state-name,Values=running --query 'Reservations[0].Instances[0].InstanceId' --output text
 }
 
 # claim_instance_from_asg claims an instance from ASG if there is no existing one claimed.
@@ -32,8 +31,7 @@ function claim_instance_from_asg() {
     # check is there any running instance already
     local instance_with_sandbox_id="$(aws ec2 describe-instances --filters Name=tag:SandboxID,Values="$SANDBOX_ID" Name=instance-state-name,Values=running)"
     if [[ "$(jq -cMr '.Reservations[0].Instances | length' <<< "$instance_with_sandbox_id")" -eq 0 ]]; then 
-        local instance_ids="$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "$ASG_NAME" --no-paginate --query "AutoScalingGroups[].Instances[].InstanceId" --output text)"
-        local instance_id="$(echo "$instance_ids" | awk '{print $1}')"
+        local instance_id="$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "$ASG_NAME" --no-paginate --query AutoScalingGroups[0].Instances[0].InstanceId --output text)"
 
         # detach-instances is an atomic operation and a single instance can not be deatched twice. 
         # This mechanism is used to prevent race condition between two or more claims that happening at the same time. The second claim would fail.
@@ -61,7 +59,7 @@ function attach_volume_if_needed() {
     aws ec2 wait volume-available --volume-ids "$volume_id"
     local volume="$(aws ec2 describe-volumes --volume-id "$volume_id")"
     [[ "$(jq -cMr ".Volumes[0].Attachments | length" <<< "$volume")" -gt 0 ]] || {
-        aws ec2 attach-volume --volume-id "$volume_id" --instance-id "$instance_id" --device "/dev/xvdf"
+        aws ec2 attach-volume --volume-id "$volume_id" --instance-id "$instance_id" --device "$DEVICE_NAME"
     }
 }
 
@@ -70,17 +68,17 @@ function retrieve_password() {
     local instance_id="$1"
     local ec2_ssh_key_file="$2"
 
-    local password_data="$(aws ec2 get-password-data --instance-id "$instance_id")"
+    local password_data="$(aws ec2 get-password-data --instance-id "$instance_id" --query 'PasswordData' --output text)"
 
     local retry_count=0
     while [[ "$retry_count" -lt "$MAX_RETRIES" ]]; do
-        password_data="$(aws ec2 get-password-data --instance-id "$instance_id" | jq -cMr .PasswordData)"
+        password_data="$(aws ec2 get-password-data --instance-id "$instance_id" --query 'PasswordData' --output text)"
         [[ -z "$password_data" ]] || break
         retry_count=$((retry_count + 1))
         sleep 10
     done
 
-    aws ec2 get-password-data --instance-id "$instance_id" | jq -r .PasswordData | base64 -d  | openssl pkeyutl  -decrypt -inkey "$ec2_ssh_key_file"
+    echo "$password_data" | base64 -d  | openssl pkeyutl  -decrypt -inkey "$ec2_ssh_key_file"
 }
 
 # validate_asg ASG_NAME
